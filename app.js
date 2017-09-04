76a11654966c1e3bfea23749240d35cdea8a4faf
Kenya Guzmán <kenyaguzmanp@gmail.com>
2017-09-04 19:09:42 -0300

    modify the style

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var path = require('path');
 var jade = require('jade');
 var upload = multer({dest:'uploads/'});
 
-//var app = module.exports = express();
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res, next) {
   res.render('index');
@@ -23,6 +23,6 @@ app.post('/upload', upload.single('file'), function(req, res, next){
 });
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 4000, function(){
     console.log('Server is running');
 });
